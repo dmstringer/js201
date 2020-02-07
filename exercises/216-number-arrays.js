fd0ed1b55257e3e754a5abc33bfd8065d80671a6
Scott <my@example.com>
2020-02-06 21:58:57 -0600

    more exercises

--- exercises/216-number-arrays.js
+++ exercises/216-number-arrays.js
@@ -1,6 +1,14 @@
 // Write a function "max" that takes an array of numbers returns the highest
 // number in the array.
-
+function max (anArray) {
+    let highNum = 0
+    for (let i = 0; i < anArray.length; i++) {
+        if (anArray[i] > highNum) {
+            highNum = anArray[i]
+        }
+    }
+    return highNum
+}
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -8,7 +16,13 @@
 // the sum of the numbers.
 // Example:
 // sumNumbers([1, 4, 8]) --> 13
-
+function sumNumbers (anArr) {
+    let numSum = 0
+    for (let i = 0; i < anArr.length; i++) {
+        numSum = numSum + anArr[i]
+    }
+    return numSum
+}
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -18,7 +32,15 @@
 // positives([1, -3, 5, -3, 0]) --> [1, 5]
 // positives([1, 2, 3]) --> [1, 2, 3]
 // positives([-1, -2, -3]) --> []
-
+function positives (arrNums) {
+    let newArr = []
+    for (let i = 0; i < arrNums.length; i++) {
+        if (arrNums[i] > 0) {
+            newArr.push(arrNums[i])
+        }
+    }
+    return newArr
+}
 
 
 
@@ -26,14 +48,30 @@
 // Write a function "evens" which takes an array of numbers and returns a new
 // array containing only the even numbers in the given array.
 // Hint: you may want to re-use your "isEven" function from 01-predicate-functions.js
-
+function evens (arrNums) {
+    let newArr = []
+    for (let i = 0; i < arrNums.length; i++) {
+        if (arrNums[i] % 2 === 0) {
+            newArr.push(arrNums[i])
+        }
+    }
+    return newArr
+}
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "odds" which takes an array of numbers and returns a new
 // array containing only the odd numbers in the given array.
 // Hint: you may want to re-use your "isOdd" function from 01-predicate-functions.js
-
+function odds (arrNums) {
+    let newArr = []
+    for (let i = 0; i < arrNums.length; i++) {
+        if (arrNums[i] % 2 !== 0) {
+            newArr.push(arrNums[i])
+        }
+    }
+    return newArr
+}
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -43,7 +81,15 @@
 //
 // Example:
 // integers([3.14, 2.4, 7, 8.1, 2]) --> [7, 2]
-
+function integers (arrNums) {
+    let newArr = []
+    for (let i = 0; i < arrNums.length; i++) {
+        if (Number.isInteger(arrNums[i])) {
+            newArr.push(arrNums[i])
+        }
+    }
+    return newArr
+}
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -52,3 +98,11 @@
 //
 // Example:
 // squareDance([1, 2, 3]) --> [1, 4, 9]
+function squareDance (arrNums) {
+    let newArr = []
+    for (let i = 0; i < arrNums.length; i++) {
+        let squNum = arrNums[i] * arrNums[i]
+        newArr.push(squNum)
+    }
+    return newArr
+}
--- exercises/218-factors.js
+++ exercises/218-factors.js
@@ -6,7 +6,45 @@
 // gcd(5, 1) --> 1
 // gcd(3, 15) --> 3
 // gcd(50, 20) --> 10
+function gcd (num1, num2) {
+    let divisorsOfNum1 = []
+    let divisorsOfNum2 = []
+    let comDivs = []
+    let grtComDiv = 0
 
+    for (let i = 0; i < num1; i++) {
+        if (num1 % (i+1) === 0) {
+            divisorsOfNum1.push((i+1))
+        }
+    }
+    console.log(divisorsOfNum1)
+    console.log('divisors of num1')
+    for (let i = 0; i < num2; i++) {
+        if (num2 % (i+1) === 0) {
+            divisorsOfNum2.push((i+1))
+        }
+    }
+    console.log(divisorsOfNum2)
+    console.log('divisors of num2')
+    for (let i = 0; i < divisorsOfNum1.length; i++) {
+        for (let i = 0; i < divisorsOfNum2.length; i++) {
+            if (divisorsOfNum1[i] === divisorsOfNum2[i]) {
+                comDivs.push(divisorsOfNum1[i])
+                break
+            }
+        }
+    }
+    console.log(comDivs)
+    console.log('common divisors')
+    for (let i = 0; i < comDivs.length; i++) {
+        if (comDivs[i] > grtComDiv) {
+            grtComDiv = comDivs[i]
+        }
+    }
+    console.log(grtComDiv)
+    console.log(' ')
+    return grtComDiv
+}
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
